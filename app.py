71922fd1a7e2a5303eda55028a7c0a78e6547883
danielsamueletukudo <danielsamueletukudo@gmail.com>
2024-03-06 11:48:17 +0100

    updated the api

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
-# app.py
+
+import os
+import uuid
 from flask import Flask, render_template, request, jsonify
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import cv2
+import tensorflow as tf
 
 app = Flask(__name__)
 
-# Load the trained model
-model = load_model('traffic_model1.h5')
+# Load the quantized TensorFlow Lite model
+tflite_quant_model = "quantized_model.tflite"
+interpreter = tf.lite.Interpreter(model_path=tflite_quant_model)
+interpreter.allocate_tensors()
 
 # Define the labels
-labels = ['barricade', 'bicycle', 'caution_bicycle', 'caution_school_zone', 'diagonal_right_turn', 'entry_prohibited',
-          'exclamation', 'horn_please', 'left_turn', 'motor_allowed', 'needless', 'no_entry', 'no_horn',
-          'no_left_or_right', 'no_motor_allowed', 'no_overtaking', 'no_right_turn', 'no_u_turn', 'pedestrian_crossing',
-          'right_turn', 'round_about', 'speed_limit_40', 'speed_limit_5', 'speed_limit_15', 'speed_limit_30',
-          'speed_limit_50', 'speed_limit_60', 'speed_limit_70', 'speed_limit_80', 'u_turn', 'x_mark']
+labels = ["No Brain tumor Detected", "Brain Tumor Detected"]
 
 
 # Function to preprocess the image
 def preprocess_image(image_path):
-    img = image.load_img(image_path, target_size=(150, 150))
+    img = image.load_img(image_path, target_size=(100, 100))
     img_array = image.img_to_array(img)
     img_array /= 255.0  # Normalize the image
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -33,26 +42,97 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
 
-    file = request.files['file']
+    if request.method == 'POST':
+            input_image = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+            if input_image.filename == '':
+                return jsonify({'response': 'No files inserted yet'})
 
-    img_path = 'uploaded_image.jpg'
-    file.save(img_path)
+            if input_image and allowed_file(input_image.filename):
 
-    # Preprocess the image
-    img_array = preprocess_image(img_path)
+                img_path = f'images/uploaded_image{uuid.uuid4()}.jpg'
 
-    # Make prediction
-    prediction = model.predict(img_array)
-    predicted_label = labels[np.argmax(prediction)]
+                input_image.save(img_path)
+                img_array = preprocess_image(img_path)
 
-    return jsonify({'prediction': predicted_label})
+                input_details = interpreter.get_input_details()
+                output_details = interpreter.get_output_details()
+
+                interpreter.set_tensor(input_details[0]['index'], img_array)
+                interpreter.invoke()
+
+                output_data = interpreter.get_tensor(output_details[0]['index'])
+                predicted_label = labels[np.argmax(output_data)]
+                return jsonify({'prediction': predicted_label})
+
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=7017,debug=True)
+    app.run(host='0.0.0.0', port=7017, debug=True)
+
+
+
+#
+#
+# # app.py
+# from flask import Flask, render_template, request, jsonify
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# import numpy as np
+#
+# app = Flask(__name__)
+#
+# # Load the trained model
+# model = load_model('traffic_model1.h5')
+#
+# # Define the labels
+# labels = ['barricade', 'bicycle', 'caution_bicycle', 'caution_school_zone', 'diagonal_right_turn', 'entry_prohibited',
+#           'exclamation', 'horn_please', 'left_turn', 'motor_allowed', 'needless', 'no_entry', 'no_horn',
+#           'no_left_or_right', 'no_motor_allowed', 'no_overtaking', 'no_right_turn', 'no_u_turn', 'pedestrian_crossing',
+#           'right_turn', 'round_about', 'speed_limit_40', 'speed_limit_5', 'speed_limit_15', 'speed_limit_30',
+#           'speed_limit_50', 'speed_limit_60', 'speed_limit_70', 'speed_limit_80', 'u_turn', 'x_mark']
+#
+#
+# # Function to preprocess the image
+# def preprocess_image(image_path):
+#     img = image.load_img(image_path, target_size=(150, 150))
+#     img_array = image.img_to_array(img)
+#     img_array /= 255.0  # Normalize the image
+#     img_array = np.expand_dims(img_array, axis=0)
+#     return img_array
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file part'})
+#
+#     file = request.files['file']
+#
+#     if file.filename == '':
+#         return jsonify({'error': 'No selected file'})
+#
+#     img_path = 'uploaded_image.jpg'
+#     file.save(img_path)
+#
+#     # Preprocess the image
+#     img_array = preprocess_image(img_path)
+#
+#     # Make prediction
+#     prediction = model.predict(img_array)
+#     predicted_label = labels[np.argmax(prediction)]
+#
+#     return jsonify({'prediction': predicted_label})
+#
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=7017,debug=True)
