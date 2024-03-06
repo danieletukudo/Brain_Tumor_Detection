3b74c5fc91febc02d9ed32adff39625567816786
danielsamueletukudo <danielsamueletukudo@gmail.com>
2024-03-06 12:07:47 +0100

    updated the api

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 #
 # @app.route('/')
 # def index():
-#     return render_template('index.html')
+#     return render_template('1index.html')
 #
 #
 # @app.route('/predict', methods=['POST'])
